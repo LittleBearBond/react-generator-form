47a90a8bd691f239ff94540a50d300c789b674ae
xiongjian <xiongjian@didichuxing.com>
2018-12-27 11:46:26 +0800

    add buildOption examples md

---
+++ buildOptions.md
@@ -0,0 +1,122 @@
+# buildOptions
+
+## use Select
+
+```js
+<Select defaultValue="0" style={{ width: 120 }}>
+    <Option value="0">全部</Option>
+    <Option value="1">已处理</Option>
+    <Option value="2">未处理</Option>
+    <Option value="3">处理完成</Option>
+</Select>
+
+//or
+const status = ['全部', '已处理', '未处理', '处理完成'];
+<Select defaultValue="0" style={{ width: 120 }}>
+    {
+        status.map((val, index) => {
+            return <Option value={index} key={val} disabled={'未处理' === val}> {val} </Option>
+        })
+    }
+</Select>
+
+const status = [
+    {
+        id: 1,
+        value: '小明'
+    },
+    {
+        id: 2,
+        value: '小李'
+    },
+    {
+        id: 3,
+        value: '小王'
+    }
+];
+
+<Select defaultValue="0" style={{ width: 120 }}>
+    {
+        status.map(({ id, value }) => <Option value={id} key={id} disabled={2 === value}> {value} </Option>)
+    }
+</Select>
+]
+```
+
+## use buildOptions
+
+```js
+import { wrapBuildOptions, buildArrOptions } from './index'
+
+const status = ['全部','已处理','未处理','处理完成']
+{
+    buildArrOptions(status, {defaultValue: '0',style:{ width: 120 }}, {disableSelect: [2]})
+}
+const status = [
+    {
+        id: 1,
+        value: '小明'
+    },
+    {
+        id: 2,
+        value: '小李'
+    },
+    {
+        id: 3,
+        value: '小王'
+    }
+];
+wrapBuildOptions(status, {defaultValue: '0',style:{ width: 120 }}, {disableSelect: [2]})
+```
+
+## [other examples](./examples/buildOptions/index.js)
+
+```js
+import { wrapBuildOptions, buildArrOptions } from './index'
+
+// init data
+const initArr = [...new Array(8).keys()];
+const arrItem = initArr.map((val) => `选项${val}`);
+
+// Array id value
+const arrIdValItem = initArr.map((val) => ({
+    id: val,
+    value: `选项${val}`,
+}));
+
+// Array key name
+const arrKeyNameItem = initArr.map((val) => ({
+    key: val,
+    name: `选项${val}`,
+}));
+
+const selectProps = {
+    defaultValue: '',
+    style: {
+        width: 200
+    }
+}
+```
+
+### buildArrOptions
+
+```js
+// 直接初始话一个数组，数组里面的值是显示项，索引是选择的值
+buildArrOptions(arrItem, selectProps)
+```
+
+### wrapBuildOptions
+
+```js
+// 直接初始话一个数组Object，数组里面item的value值是显示项，id是选择的值
+// 第二个参数为select的 props
+wrapBuildOptions(arrIdValItem, { ...selectProps, mode: 'multiple', defaultValue: [], placeholder: '请选择，可多选' })
+```
+
+### wrapBuildOptions key:key  value:name
+
+```js
+// 初始话一个数组Object，数组里面item的value值是显示项，key是选择的值
+// 默认id是选之后提交的值，value是显示项，但是可以指定相应的key
+wrapBuildOptions(arrKeyNameItem, selectProps, { key: 'key', value: 'name', disableSelect:[3,5]})
+```
---
+++ examples/buildOptions/index.js
@@ -0,0 +1,159 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ItemGenerator, { wrapBuildOptions, buildArrOptions } from '../../index'
+import { Card, Form, Row, Col } from 'antd';
+
+import 'antd/dist/antd.css';
+
+const initArr = [...new Array(8).keys()];
+const arrItem = initArr.map((val) => `选项${val}`);
+
+const arrIdValItem = initArr.map((val) => ({
+    id: val,
+    value: `选项${val}`,
+}));
+
+const arrKeyNameItem = initArr.map((val) => ({
+    key: val,
+    name: `选项${val}`,
+}));
+
+const selectProps = {
+    defaultValue: '',
+    style: {
+        width: 200
+    }
+}
+
+const requiredRules = {
+    rules: [
+        { required: true, message: '请选择' }
+    ]
+}
+
+const items = [
+    {
+        item: {
+            id: 'select1',
+            type: 'select',
+            label: 'arr select',
+            options: {
+                initialValue: '',
+                ...requiredRules
+            },
+            // 直接使用数组，数组的index 会作为value提交到后端
+            data: arrItem,
+        }
+    },
+    {
+        item: {
+            id: 'select2',
+            type: 'select',
+            label: 'arrIdValueItem',
+            options: {
+                initialValue: '',
+                ...requiredRules
+            },
+            // 直接使用数组，数组的index 会作为value提交到后端
+            data: arrIdValItem,
+        }
+    },
+    {
+        item: {
+            id: 'select3',
+            type: 'select',
+            label: 'arrKeyNameItem',
+            options: {
+                initialValue: '',
+                ...requiredRules
+            },
+            data: arrKeyNameItem,
+            params: {
+                key: 'key',
+                value: 'name'
+            }
+        }
+    },
+    {
+        item: {
+            id: 'select3',
+            type: 'select',
+            label: 'disableSelect',
+            options: {
+                initialValue: '',
+                ...requiredRules
+            },
+            data: arrKeyNameItem,
+            params: {
+                key: 'key',
+                value: 'name',
+                disableSelect: [3, 5]
+            }
+        }
+    },
+]
+
+class TestBuildOption extends React.Component {
+    render() {
+        return <Card title="buildArrOptions demo">
+            <Form>
+                <Row>
+                    <Col>
+                        <h1>buildArrOptions</h1>
+                        {
+                            /**直接初始话一个数组，数组里面的值是显示项，索引是选择的值 */
+                            buildArrOptions(arrItem, selectProps)
+                        }
+                    </Col>
+                    <Col>
+                        <h1>wrapBuildOptions</h1>
+                        {
+                            /**直接初始话一个数组Object，数组里面item的value值是显示项，id是选择的值 */
+                            wrapBuildOptions(arrIdValItem, { ...selectProps, mode: 'multiple', defaultValue: [], placeholder: '请选择，可多选' })
+                        }
+                        <br />
+                    </Col>
+                    <Col>
+                        <h1>wrapBuildOptions disableSelect</h1>
+                        {
+                            /**直接初始话一个数组Object，数组里面item的value值是显示项，id是选择的值 */
+                            wrapBuildOptions(arrIdValItem, { ...selectProps, mode: 'multiple', defaultValue: [], placeholder: '请选择，可多选', disableSelect: [3, 5] })
+                        }
+                        <br />
+                    </Col>
+                    <Col>
+                        <h1>wrapBuildOptions ; key:'key'  value:'name'</h1>
+                        {
+                            /**直接初始话一个数组Object，数组里面item的value值是显示项，key是选择的值 */
+                            /** 默认id是选之后提交的值，但是可以指定相应的key*/
+                            wrapBuildOptions(arrKeyNameItem, selectProps, { key: 'key', value: 'name' })
+                        }
+                        <br />
+                    </Col>
+                    <Col>
+                        <h1>wrapBuildOptions = wrapBuildOptions；key:'key'  value:'name'</h1>
+                        {
+                            /**直接初始话一个数组Object，数组里面item的value值是显示项，key是选择的值 */
+                            /** 默认id是选之后提交的值，但是可以指定相应的key*/
+                            wrapBuildOptions(arrKeyNameItem, selectProps, { key: 'key', value: 'name' })
+                        }
+                        <br />
+                        <br />
+                    </Col>
+
+                    <ItemGenerator
+                        form={this.props.form}
+                        options={{
+                            items
+                        }}
+                    />
+                </Row>
+            </Form>
+        </Card>
+    }
+}
+TestBuildOption = Form.create()(TestBuildOption)
+ReactDOM.render(<TestBuildOption />, document.getElementById('root'));
+
+
+
